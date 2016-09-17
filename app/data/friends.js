33cf1dd0d22ff574bc4b5646c94141175210d1ed
Jesus Cuza <Jcuza@knights.ucf.edu>
2016-09-17 11:33:42 -0400

    Second iteration of homework

---
+++ app/data/friends.js
@@ -0,0 +1,33 @@
+var friendsArray = [{
+  "name":"Jesus",
+  "photo":"https://scontent.xx.fbcdn.net/v/t1.0-9/12540803_1288537004496710_3491038515875975344_n.jpg?oh=6eebd628bef1d627a9165f970e45cd2d&oe=5873C6BB",
+  "scores":[
+     5,
+     1,
+     4,
+     4,
+     5,
+     1,
+     2,
+     5,
+     4,
+     1
+   ]
+},{
+  "name":"Red",
+  "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/AAEAAQAAAAAAAAVvAAAAJDhhNjcyODljLThjY2ItNGNjYi04M2YwLTE0M2RmYjQyMjdhOQ.jpg",
+  "scores":[
+     5,
+     5,
+     5,
+     5,
+     5,
+     5,
+     5,
+     5,
+     5,
+     5
+   ]
+}];
+
+module.exports = friendsArray;
---
+++ app/routing/api-routes.js
@@ -0,0 +1,38 @@
+var friends = require('../data/friends.js');
+
+module.exports = function(app){
+	//This function gets the friends data when the API Friends list link is clicked on
+	app.get('/api/friends', function(req, res){
+		res.send(friends);
+	});
+
+	//This function adds a new user
+	//Req.body is the incoming survey data.
+	app.post('/api/friends', function(req, res){
+
+		var newFriend = req.body;
+		friends.push(newFriend);
+
+		var totalDifference = 0;
+		var allDifferences = [];
+
+		//Loop through all of the stored friends (minus one because the last stored friend is the current user)
+		for (var i=0; i<(friends.length-1); i++){
+
+			//Loop through all of question values and sum total their subtracted absolute values
+			for (var j=0; j<10; j++){
+				totalDifference += Math.abs(friends[i].scores[j] - newFriend.scores[j]);
+			}
+
+			allDifferences.push(totalDifference);
+			totalDifference = 0;
+		}
+
+		//Give the smallest value in the array
+		var bestMatch = friends[allDifferences.indexOf(Math.min.apply(null, allDifferences))];
+
+		res.send(bestMatch);
+
+	});
+
+}

b4a5d8f8204deba461d6275031538759d154b430
Jesus Cuza <Jcuza@knights.ucf.edu>
2016-09-18 20:11:23 -0400

    third iteration of homework

--- app/data/friends.js
+++ app/data/friends.js
@@ -2,31 +2,31 @@ var friendsArray = [{
   "name":"Jesus",
   "photo":"https://scontent.xx.fbcdn.net/v/t1.0-9/12540803_1288537004496710_3491038515875975344_n.jpg?oh=6eebd628bef1d627a9165f970e45cd2d&oe=5873C6BB",
   "scores":[
-     5,
-     1,
-     4,
-     4,
-     5,
-     1,
+     3,
      2,
-     5,
+     1,
      4,
-     1
+     2,
+     3,
+     4,
+     4,
+     5,
+     2
    ]
 },{
   "name":"Red",
   "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/AAEAAQAAAAAAAAVvAAAAJDhhNjcyODljLThjY2ItNGNjYi04M2YwLTE0M2RmYjQyMjdhOQ.jpg",
   "scores":[
+     1,
+     2,
+     3,
+     4,
      5,
-     5,
-     5,
-     5,
-     5,
-     5,
-     5,
-     5,
-     5,
-     5
+     4,
+     3,
+     2,
+     1,
+     4
    ]
 }];
 
